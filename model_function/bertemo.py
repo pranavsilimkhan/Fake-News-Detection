6bbbb797b912a819e04c25030e675dcbae6d6838
Pramod Karkhani <43420089+pramod-karkhani@users.noreply.github.com>
2023-10-20 23:35:08 -0400

    Add files via upload

---
+++ model_function/bertemo.py
@@ -0,0 +1,122 @@
+import os
+import torch
+import tqdm
+import torch.nn as nn
+from .layers import *
+from sklearn.metrics import *
+from transformers import BertModel
+from utils.utils import data2gpu, Averager, metrics, Recorder
+from utils.dataloader import get_dataloader
+
+class BERTEmoModel(torch.nn.Module):
+    def __init__(self, emb_dim, mlp_dims, dropout):
+        super(BERTEmoModel, self).__init__()
+        self.bert = BertModel.from_pretrained('bert-base-uncased').requires_grad_(False)
+
+        for name, param in self.bert.named_parameters():
+            #param.requires_grad = True
+            if name.startswith("encoder.layer.11"): \
+                    # or name.startswith('encoder.layer.10') \
+                    # or name.startswith('encoder.layer.9'): \
+                    # or name.startswith('encoder.layer.8') \
+                    # or name.startswith('encoder.layer.7') \
+                    # or name.startswith('encoder.layer.6')\
+                    # or name.startswith('encoder.layer.5') \
+                    # or name.startswith('encoder.layer.4')\
+                    # or name.startswith('encoder.layer.3'):
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+
+        self.fea_size = emb_dim
+        self.mlp = MLP(emb_dim * 2 + 47, mlp_dims, dropout)
+        self.rnn = nn.GRU(input_size = emb_dim,
+                          hidden_size = self.fea_size,
+                          num_layers = 1, 
+                          batch_first = True, 
+                          bidirectional = True)
+        self.attention = MaskAttention(emb_dim * 2)
+    
+    def forward(self, **kwargs):
+        inputs = kwargs['content']
+        masks = kwargs['content_masks']
+        emotion = kwargs['emotion']
+        bert_feature = self.bert(inputs, attention_mask = masks)[0]
+        feature, _ = self.rnn(bert_feature)
+        feature, _ = self.attention(feature, masks)
+        output = self.mlp(torch.cat([feature, emotion], dim = 1))
+        return torch.sigmoid(output.squeeze(1))
+
+
+
+def test(dataloader, model, config):
+    pred = []
+    label = []
+    model.eval()
+    data_iter = tqdm.tqdm(dataloader)
+    for step_n, batch in enumerate(data_iter):
+        with torch.no_grad():
+            batch_data = data2gpu(batch, config['use_cuda'])
+            batch_label = batch_data['label']
+            batch_pred = model(**batch_data)
+
+            label.extend(batch_label.detach().cpu().numpy().tolist())
+            pred.extend(batch_pred.detach().cpu().numpy().tolist())
+    
+    return metrics(label, pred)
+
+def train_bert(config, logger):
+    save_path = os.path.join(config['save_param_dir'], config['model_name'])
+    if os.path.exists(save_path):
+        save_param_dir = save_path
+    else:
+        save_param_dir = os.makedirs(save_path)
+
+
+    if(logger):
+        logger.info('start training......')
+    model = BERTEmoModel(config['emb_dim'], config['model']['mlp']['dims'], config['model']['mlp']['dropout'])
+    if config['use_cuda']:
+        model = model.cuda()
+    loss_fn = torch.nn.BCELoss()
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=config['lr'], weight_decay=config['weight_decay'])
+    recorder = Recorder(config['early_stop'])
+    val_loader = get_dataloader(config['root_path'] + 'val.json', config['max_len'], config['batchsize'], shuffle=False, use_endef=False aug_prob=config['aug_prob'])
+
+    for epoch in range(config['epoch']):
+        model.train()
+        train_loader = get_dataloader(config['root_path'] + 'train.json', config['max_len'], config['batchsize'], shuffle=True, use_endef=False, aug_prob=config['aug_prob'])
+        train_data_iter = tqdm.tqdm(train_loader)
+        avg_loss = Averager()
+
+        for step_n, batch in enumerate(train_data_iter):
+            batch_data = data2gpu(batch, config['use_cuda'])
+            label = batch_data['label']
+
+            pred = model(**batch_data)
+            loss = loss_fn(pred, label.float())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            avg_loss.add(loss.item())
+        print('Training Epoch {}; Loss {}; '.format(epoch + 1, avg_loss.item()))
+
+        results = test(val_loader, model, config)
+        mark = recorder.add(results)
+        if mark == 'save':
+            torch.save(model.state_dict(),
+                os.path.join(save_path, 'parameter_bert.pkl'))
+        elif mark == 'esc':
+            break
+        else:
+            continue
+    model.load_state_dict(torch.load(os.path.join(save_path, 'parameter_bertemo.pkl')))
+
+    test_future_loader = get_dataloader(config['root_path'] + 'test.json', config['max_len'], config['batchsize'], shuffle=False, use_endef=False, aug_prob=config['aug_prob'])
+    future_results = test(test_future_loader, model, config)
+    if(logger):
+        logger.info("start testing......")
+        logger.info("test score: {}.".format(future_results))
+        logger.info("lr: {}, aug_prob: {}, avg test score: {}.\n\n".format(config['lr'], config['aug_prob'], future_results['metric']))
+    print('test results:', future_results)
+    return future_results, os.path.join(save_path, 'parameter_bertemo.pkl')
--- model_function/layers.py
+++ model_function/layers.py
@@ -4,88 +4,49 @@ import torch.nn.functional as F
 from torch.autograd import Function
 import math
 
-class ReverseLayerF(Function):
-    @staticmethod
-    def forward(ctx, input_, alpha):
-        ctx.alpha = alpha
-        return input_
+def reverse_layer_f(input_, alpha):
+    return input_
 
-    @staticmethod
-    def backward(ctx, grad_output):
-        output = grad_output.neg() * ctx.alpha
-        return output, None
+def mlp(input_dim, embed_dims, dropout, output_layer=True):
+    layers = []
+    for embed_dim in embed_dims:
+        layers.append(nn.Linear(input_dim, embed_dim))
+        layers.append(nn.ReLU())
+        layers.append(nn.Dropout(p=dropout))
+        input_dim = embed_dim
+    if output_layer:
+        layers.append(nn.Linear(input_dim, 1)
+    return nn.Sequential(*layers)
 
-class MLP(torch.nn.Module):
-
-    def __init__(self, input_dim, embed_dims, dropout, output_layer=True):
-        super().__init__()
-        layers = list()
-        for embed_dim in embed_dims:
-            layers.append(torch.nn.Linear(input_dim, embed_dim))
-            layers.append(torch.nn.ReLU())
-            layers.append(torch.nn.Dropout(p=dropout))
-            input_dim = embed_dim
-        if output_layer:
-            layers.append(torch.nn.Linear(input_dim, 1))
-        self.mlp = torch.nn.Sequential(*layers)
-
-    def forward(self, x):
-        """
-        :param x: Float tensor of size ``(batch_size, embed_dim)``
-        """
-        return self.mlp(x)
-
-class cnn_extractor(nn.Module):
-    def __init__(self, feature_kernel, input_size):
-        super(cnn_extractor, self).__init__()
-        self.convs = torch.nn.ModuleList(
-            [torch.nn.Conv1d(input_size, feature_num, kernel)
-             for kernel, feature_num in feature_kernel.items()])
-
-    def forward(self, input_data):
+def cnn_extractor(feature_kernel, input_size):
+    convs = [nn.Conv1d(input_size, feature_num, kernel) for kernel, feature_num in feature_kernel.items()]
+    
+    def forward(input_data):
         share_input_data = input_data.permute(0, 2, 1)
-        feature = [conv(share_input_data) for conv in self.convs]
-        feature = [torch.max_pool1d(f, f.shape[-1]) for f in feature]
+        feature = [conv(share_input_data) for conv in convs]
+        feature = [F.max_pool1d(f, f.shape[-1]) for f in feature]
         feature = torch.cat(feature, dim=1)
-        feature = feature.view([-1, feature.shape[1]])
+        feature = feature.view([-1, feature.shape[1])
         return feature
+    
+    return forward
 
-class MaskAttention(torch.nn.Module):
-    """
-    Compute attention layer
-    """
-    def __init__(self, input_shape):
-        super(MaskAttention, self).__init__()
-        self.attention_layer = torch.nn.Linear(input_shape, 1)
-
-    def forward(self, inputs, mask=None):
-        # print("inputs: ", inputs.shape)     #(128, 170, 768)
-        scores = self.attention_layer(inputs).view(-1, inputs.size(1))
-        # print("scores: ", scores.shape)     #(128, 170)
+def mask_attention(input_shape):
+    attention_layer = nn.Linear(input_shape, 1)
+    
+    def forward(inputs, mask=None):
+        scores = attention_layer(inputs).view(-1, inputs.size(1))
         if mask is not None:
             scores = scores.masked_fill(mask == 0, float("-inf"))
-        scores = torch.softmax(scores, dim=-1).unsqueeze(1)
-        # print("scores: ", scores.shape)     #(128, 1, 170)
+        scores = F.softmax(scores, dim=-1).unsqueeze(1)
         outputs = torch.matmul(scores, inputs).squeeze(1)
-        # print("outputs: ", outputs.shape)   #(128, 768)
-
         return outputs, scores
 
-class Attention(torch.nn.Module):
-    """
-    Compute 'Scaled Dot Product Attention
-    """
-
-    def forward(self, query, key, value, mask=None, dropout=None):
-        scores = torch.matmul(query, key.transpose(-2, -1)) \
-                 / math.sqrt(query.size(-1))
-
-        if mask is not None:
-            scores = scores.masked_fill(mask == 0, float("-inf"))
-
-        p_attn = F.softmax(scores, dim=-1)
-
-        if dropout is not None:
-            p_attn = dropout(p_attn)
-
-        return torch.matmul(p_attn, value), p_attn
+def attention(query, key, value, mask=None, dropout=None):
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(query.size(-1))
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, float("-inf"))
+    p_attn = F.softmax(scores, dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
